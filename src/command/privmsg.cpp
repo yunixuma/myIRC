83178fa48d465cba6740aabbfe5beef3168a2fe7
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2023-08-12 19:13:38 +0900

    revised

--- src/command/privmsg.cpp
+++ src/command/privmsg.cpp
@@ -1,16 +1,16 @@
 
 #include "server.h"
+#include "client.h"
 
 	// privmsg関数内で、parametersベクトルの中身を調べ、
 	// 	チャンネルへのメッセージ：PRIVMSG #channel :Hello, everyone!
 	// 個人へのメッセージ：PRIVMSG nickname :Hello, how are you?
-
-	void Server::privmsg(const std::vector<std::string>& parameters)
-	{
 		using std::string;
 		using std::cout;
 		using std::endl;
 
+	void Server::privmsg(const std::vector<std::string>& parameters)
+	{
 	if (parameters.size() < 2) {
     // エラーメッセージを返すなど、パラメータが不足している場合の処理
     	return;
@@ -23,20 +23,20 @@
 
     if (destination[0] == '#') {
         // チャンネルへのメッセージの処理
-        
 		// この部分では、チャンネルの全てのメンバーにメッセージを送信するロジックを実装する
-
         // 例えば、findChannelByName関数でチャンネルを検索し、そのメンバーにメッセージをループで送信するなど
-
-    } else {
+    } else {else {
         // 個人へのメッセージの処理
-      
-		// この部分では、指定されたユーザー名でユーザーを検索し、そのユーザーにメッセージを送信するロジックを実装する
-    
-	}
+        std::string targetUsername = destination; // 送信先のユーザー名
+
+        // ログインしたユーザーを管理する vector から該当するユーザーを検索
+        for (std::vector<Client>::const_iterator it = loggedInUsers.begin(); it != loggedInUsers.end(); ++it) {
+    const Client& user = *it; {
+            if (user.getUserName() == targetUsername) {
+                // ユーザーにメッセージを送信するロジックを実装
+                user.distributeMessage(*this, message); // Serverインスタンスを渡す例
+                break; // ユーザーを見つけたらループを終了
+            }
+        }
+    }
 }
-	// #があった場合は、チャンネルのユーザー向けにメッセージを送り、
-	// なかった場合は個人にメッセージを送る
-
-	// 宛先とメッセージ本文を適切に処理するロジックが必要になるでしょう。
-
