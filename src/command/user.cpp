89f758d4f289a3402b50cda132819c739351c2b9
ykosaka <ykosaka@student.42tokyo.jp>
2023-08-20 12:56:36 +0900

    Add nick, user command

--- src/command/user.cpp
+++ src/command/user.cpp
@@ -1,12 +1,12 @@
 #include "Command.hpp"
 
-int	Command::nick(Client& client, Channe& channel, const Message& msg) {
+int	Command::user(Client& client, Channe& channel, const Message& msg) {
 	std::clog << "\033[36;2;3m[" << this \
 		<< "]<Command> nick called (" << client->username_ << ")\033[m" << std::endl;
 	std::vector<std::string>	params = msg.getParameters();
 	if (!params)
 		return (1);
-	client.setNickname(*params[0]);
+	client.setUserName(*params[0]);
 	return (0);
 	(void)channel;
 }
