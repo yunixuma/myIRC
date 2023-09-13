5aa563d5b3818d2ffc2fe2b79267353693b8dcd9
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2023-09-13 18:21:42 +0900

    repair bugs and revised the client method

--- src/command/privmsg.cpp
+++ src/command/privmsg.cpp
@@ -8,9 +8,13 @@
 		using std::cout;
 		using std::endl;
 
+
 	void Server::privmsg(Client &user_, Channel &channel_, const Message &message_)
 	{
-		if (parameters.size() < 2) {
+		const std::vector<std::string>& parameters = message_.getParameters();
+		int size = message_.getParameters().size() ;
+
+		if (size < 2) {
 		// エラーメッセージを返すなど、パラメータが不足している場合の処理
 			return;
 		}
@@ -26,8 +30,8 @@
 		// この部分では、チャンネルの全てのメンバーにメッセージを送信するロジックを実装する
         // 例えば、findChannelByName関数でチャンネルを検索し、そのメンバーにメッセージをループで送信するなど
     } else {
-		user_find() = destanation[1];
-		send().message();
+		// user_find() = destination[1];
+		// send().message();
 		
 		// 個人へのメッセージの処理
 		// この部分では、指定されたユーザー名でユーザーを検索し、
