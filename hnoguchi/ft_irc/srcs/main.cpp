2f19342df1b52ee898a9e81f30c451892dfbb980
hnoguchi <>
2023-09-29 09:28:02 +0900

    Server classのrun();メンバ関数を修正

--- hnoguchi/ft_irc/srcs/main.cpp
+++ hnoguchi/ft_irc/srcs/main.cpp
@@ -1,24 +1,5 @@
 #include "Server.hpp"
 
-int	getline_prompt(std::string prompt, std::string *line)
-{
-	char	buffer[111];
-
-	std::cout << prompt;
-	std::cin.clear(std::ios::goodbit);
-	std::cin.width(110);
-	std::cin.getline(buffer, 110);
-	if (std::cin.eof() == true) {
-		std::cout << "\nlog out." << std::endl;
-		return (-1);
-	} else if (std::cin.fail() == true) {
-		std::cout << "\nPhoneBook: Fatal error: std::cin" << std::endl;
-		return (-1);
-	}
-	*line = buffer;
-	return (0);
-}
-
 void	construct_test_env(Server& server)
 {
 	server.pushClient(1);
@@ -43,27 +24,9 @@ void	destruct_test_env(Server& server)
 int	main()
 {
 	Server		server(8080);
-	std::string	message;
 
-	server.run();
 	construct_test_env(server);
-	while (server.getRunning()) {
-		if (getline_prompt("Message: ", &message) < 0) {
-			server.stop();
-		}
-		if (message == "QUIT") {
-			server.stop();
-		}
-		if (message == "JOIN") {
-			static_cast<Join*>(server.commandList_["JOIN"])->pushClient(*(server.searchChannel("sample_1")), *(server.searchClient(1)));
-			static_cast<Join*>(server.commandList_["JOIN"])->pushClient(*(server.searchChannel("sample_1")), *(server.searchClient(2)));
-			static_cast<Join*>(server.commandList_["JOIN"])->pushClient(*(server.searchChannel("sample_1")), *(server.searchClient(3)));
-		}
-		else {
-			std::cout << message << std::endl;
-		}
-		server.debugList();
-	}
+	server.run();
 	destruct_test_env(server);
 #ifdef LEAKS
 	system("leaks -q ircserv");
